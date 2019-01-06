ffa925744cc82ef00a5e963e79533d86aec8e237
lulldev <lulldev@yandex.ru>
2019-01-06 15:01:46 +0300

    make test with building minify js

--- lzo/build.sh
+++ lzo/build.sh
@@ -3,4 +3,4 @@
 rm -rf build
 mkdir build
 source /Users/ivankalagin/Documents/tmp/emsdk/emsdk_env.sh
-emcc -o build/index.html testmini.c minilzo.c -O3 -s WASM=1 -s NO_EXIT_RUNTIME=1 --shell-file template.html
+emcc -o build/index.html testmini.c minilzo.c -O3 -s WASM=1 -s NO_EXIT_RUNTIME=1 --shell-file template.html --preload-file bundle/target.compressed.js
